2d8f26ee2c659416957b4b616078fa9372840326
Marc <marclopez23496@gmail.com>
2020-04-18 23:19:26 +0200

    Final

--- js/js.js
+++ js/js.js
@@ -1,12 +1,17 @@
 
-var pattern = "^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$";
+var pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
 function getEmail(){
   str= document.getElementById('email').value;
-
+  console.log(str);
   if(str.match(pattern)){
-    console.log("ok");
+    document.getElementById("fallo").style.display= "none";
+    document.querySelector(".correo").style.borderColor = "hsl(0, 36%, 70%)";
+    document.querySelector(".correo").style.color = "hsla(0, 36%, 70%, 0.5)";
   } else{
     document.getElementById("fallo").innerHTML = "Please provide a valid email";
+    document.querySelector(".correo").style.borderColor = "hsl(0, 93%, 68%)";
+    document.querySelector(".correo").style.color = "#000000";
   }
 
 }
